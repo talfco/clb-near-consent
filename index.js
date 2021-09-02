f6677ce2781e867a8353cda80be8161918ee2675
Felix Kuestahler <talfco@gmail.com>
2021-09-02 22:33:01 +0200

    Update

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 require('global');
 const process = require('process');
 const minimist = require('minimist');
-const { Web3Storage, getFilesFromPath } = require('web3.storage');
-const fs = require('fs').promises;
-
+const { Web3Storage, File } = require('web3.storage');
 const sssa = require('./module/sssa.js');
-
-bigInt = require('big-integer');
 
 function logger(text) {
     if (typeof document === "undefined") {
@@ -15,7 +11,6 @@ function logger(text) {
         document.write(text + "<br>");
     }
 }
-
 function assertEqual(actual, expected) {
     if (typeof actual === typeof bigInt(0)) {
         if (actual.compare(expected) !== 0) {
@@ -31,23 +26,21 @@ function assertEqual(actual, expected) {
     return true;
 }
 
-async function openFile(share, index) {
-    try {
 
-        await fs.writeFile('./share-".csv', csvHeaders);
-    } catch (error) {
-        console.error(`Got an error trying to write to a file: ${error.message}`);
-    }
-}
 
 async function main() {
     var shares = sssa.create(4, 5, "Hello World from Talfco")
     var files=[];
+
     for (var index in shares) {
         logger("Got share to store "+index+": "+shares[index]);
-        files[index] = new global.File.constructor([shares[index]], 'plain-utf8.txt');
-    }
+        try {
+            files[index] = new File([ shares[index]],'share-'+index+'.txt')
+        } catch (err) {
+            console.error(err)
+        }
 
+    }
     var secret = sssa.combine(shares)
     logger("Got back secret ("+shares.length+"):"+secret)
     const sharesreduced = shares.slice(0,4)
